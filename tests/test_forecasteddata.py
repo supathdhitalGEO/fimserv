26178e4ae1b2d6745d8adc6a1d3ec297965225ce
Supath Dhital <92934304+supathdhitalGEO@users.noreply.github.com>
2024-11-12 01:31:34 -0600

    updating the test script for forecasted streamflow

--- tests/test_forecasteddata.py
+++ tests/test_forecasteddata.py
@@ -5,11 +5,15 @@ import owphandfim as fm
 huc = "03020202"
 
 # mention the forecast range as shortrange or mediumrange or longrange
-# Download the data
-# fm.getNWMForecasteddata(huc, forecast_range='shortrange')
+# Download the data if user wants to download the data for fixed date and hour otherwise it will download the data by taking current date and time fore forecasted data
+# fm.getNWMForecasteddata(huc, forecast_range='shortrange', forecast_date='2021-09-01', hour = 6)
+
+#For current date and time forecasted data
+fm.getNWMForecasteddata(huc, forecast_range='shortrange')
+
 
 # By default the data will be filtered based on maximum discharge value
 # Otherwise user need to mention
 
-fm.getNWMForecasteddata(huc, forecast_range="shortrange", sort_by="maximum")
-fm.getNWMForecasteddata(huc, forecast_range="longrange", sort_by="minimum")
+# fm.getNWMForecasteddata(huc, forecast_range="shortrange", sort_by="maximum")
+# fm.getNWMForecasteddata(huc, forecast_range="longrange", sort_by="minimum")
